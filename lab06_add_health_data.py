42dbd597bd54973572c94f149f82f8d7b1fd122b
CJ O'Sullivan <me@cjosullivan.net>
2018-03-05 19:16:43 +0000

    Lab06 Completed

--- lab06_add_health_data.py
+++ lab06_add_health_data.py
@@ -33,3 +33,47 @@ data['NUMETHNIC'] = data['H1GI4'] + data['H1GI6A'] + data['H1GI6B'] + data['H1GI
 count1=data['NUMETHNIC'].value_counts(sort=True)
 print(count1)
 
+#### Lab06 Section 3 ####
+
+def ETHNICITY (row):
+    if row['NUMETHNIC'] > 1:
+        return 1
+    if row['H1GI4'] == 1:
+        return 2
+    if row['H1GI6A'] == 1:
+        return 3
+    if row['H1GI6B'] == 1:
+        return 4
+    if row['H1GI6C'] == 1:
+        return 5
+    if row['H1GI6D'] == 1:
+        return 6
+    
+data['ETHNICITY'] = data.apply(lambda row: ETHNICITY (row), axis=1)
+
+subset1 = data[['AID', 'H1GI4', 'H1GI6A', 'H1GI6B', 'H1GI6C', 'H1GI6D', 'NUMETHNIC', 'ETHNICITY']]
+subset1.head(25)
+
+#### Lab06 Section 4 ####
+
+## Quartile split qcut function into four groups.
+print('AGE - 4 Categories - Quartiles')
+subset2['AGEGROUP'] = pandas.qcut(subset2.AGE, 4, labels=['1=25%tile','2=50%tile','3=75%tile','4=100%tile'])
+c14 = subset2['AGEGROUP'].value_counts(sort=False, dropna=True)
+print(c14)
+
+## Categorise variable based on customised splits using the cut() functions. 
+## Splits into three groups, 18 - 20, 21 - 22 and 23- 25
+subset2['AGEGROUP2']=pandas.cut(subset2.AGE,[17, 20, 22, 25], labels=['18-20','21-22','23-25'])
+c15 = subset2['AGEGROUP2'].value_counts(sort=False, dropna= True)
+print(c15)
+
+subset2['AGEGROUP2']=pandas.cut(subset2.AGE,[17, 20, 22, 25], labels=['18-20','21-22','23-25'])
+c16 = subset2['AGEGROUP2'].value_counts(sort=True, dropna= True, normalize= True)
+print(c16)
+
+
+
+        
+        
+
